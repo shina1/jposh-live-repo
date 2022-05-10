ff6720523ce501a07aaac41df68de21ef3d7d9cd
shina1 <oswasiu@gmail.com>
2022-05-10 16:43:16 +0100

    updating to secure base url

--- client/src/utils/requestMethods.js
+++ client/src/utils/requestMethods.js
@@ -1,18 +1,9 @@
-import axios from "axios"
 
 
-export const PRODUCTION_BASE_URL = "http://141.136.42.247:5500/api/v1/"
+
+export const PRODUCTION_BASE_URL = "https://141.136.42.247:5500/api/v1/"
 
 // export const PRODUCTION_BASE_URL = "http://localhost:5500/api/v1/"
 // export const PRODUCTION_BASE_URL = "https://ancient-beach-60604.herokuapp.com/api/v1/"
-
-// export const publicRequest = axios.create({
-//     baseURL: PRODUCTION_BASE_URL,
-// });
-
-// export const userRequest = axios.create({
-//     baseURL: PRODUCTION_BASE_URL,
-//     header: {token: `Bearer ${TOKEN}`},
-// })
 
 // checking for updates
