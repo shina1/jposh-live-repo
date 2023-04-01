21a06d060fe5c8022d89aa0f61725394866ef976
shina1 <oswasiu@gmail.com>
2023-04-01 15:06:10 +0100

    UI: completed article card page

--- client/src/Components/Blog/BlogArticles.js
+++ client/src/Components/Blog/BlogArticles.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import ArticleCards from './ArticleCards'
+import React from "react";
+import ArticleCards from "./ArticleCards";
+import {FaArrowRight} from 'react-icons/fa';
 
 const BlogArticles = () => {
   return (
-    <section className='m-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
-        <ArticleCards className="w-full md:w-[calc(50%-20px)] lg:w-[calc(30%-20px)]"/>
-        <ArticleCards className="w-full md:w-[calc(50%-20px)]
-        lg:w-[calc(33.33%-21px)]"/>
-        <ArticleCards className="w-full md:w-[calc(50%-20px)]
-        lg:w-[calc(33.33%-21px)]"/>
-        <ArticleCards className="w-full md:w-[calc(50%-20px)]
-        lg:w-[calc(33.33%-21px)]"/>
-      
+    <section className="flex flex-col container m-auto px-5 py-10">
+      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+        <ArticleCards className="w-full md:w-[calc(50%-20px)] lg:w-[calc(30%-20px)]" />
+        <ArticleCards
+          className="w-full md:w-[calc(50%-20px)]
+        lg:w-[calc(33.33%-21px)]"
+        />
+        <ArticleCards
+          className="w-full md:w-[calc(50%-20px)]
+        lg:w-[calc(33.33%-21px)]"
+        />
+        <ArticleCards
+          className="w-full md:w-[calc(50%-20px)]
+        lg:w-[calc(33.33%-21px)]"
+        />
+      </div>
+      <button className="flex flex-row mx-auto items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg"><span className="text-primary">More articles</span> <FaArrowRight size={15} /></button>
     </section>
-  )
-}
+  );
+};
 
-export default BlogArticles
+export default BlogArticles;
